15379942ae7cb23a550bd1310f34a66553a01e24
Alex Joshua <ialexjoshua@gmail.com>
2022-07-22 20:36:27 +0200

    Updates

---
+++ precache-manifest.3273cdd51b18498d0c139c737b7acc7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3de920ee951bb8fdcd0f",
+    "url": "/wheather-app/static/css/main.dfd39139.chunk.css"
+  },
+  {
+    "revision": "3de920ee951bb8fdcd0f",
+    "url": "/wheather-app/static/js/main.3de920ee.chunk.js"
+  },
+  {
+    "revision": "f696c4db138f4966f215",
+    "url": "/wheather-app/static/css/1.a3d7f650.chunk.css"
+  },
+  {
+    "revision": "f696c4db138f4966f215",
+    "url": "/wheather-app/static/js/1.f696c4db.chunk.js"
+  },
+  {
+    "revision": "93a3ef3197e0b5ddc0a1",
+    "url": "/wheather-app/static/js/runtime~main.93a3ef31.js"
+  },
+  {
+    "revision": "e4cc4726b4a07b0fac76ab0c910ddc82",
+    "url": "/wheather-app/index.html"
+  }
+];
